d8fb22fcf25213262b764e19f947be6902eb1aa6
sibrahim7 <sibrahim@moddakir.com>
2026-08-04 16:03:24 +0300

    chore: release 1.0.1

---
+++ example/android/app/src/main/kotlin/com/moddakir/moddakir_flutter_plugin_example/ModdakirApp.kt
@@ -0,0 +1,13 @@
+package com.moddakir.moddakir_flutter_plugin_example
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ModdakirApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
